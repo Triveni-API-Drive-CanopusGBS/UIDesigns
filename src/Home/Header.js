f2771dd86984433cdd99b9c32aedd6f53dcbaf38
Gangadhar <gangadhar.reddy@canopusgbs.com>
2024-07-25 16:36:06 +0530

    Concise dropdown

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -9,9 +9,7 @@ function Header() {
             <div className="logo">Triveni Turbines</div>
             <nav className="navbar">
                 <div className="container">
-                    <Link className="nav-link" to="/create-quotation">Create New Quote</Link>
-                    <Link className="nav-link" to="/edit-quotations">My Quotations <span>&#x25BC;</span></Link>
-                    <Link className="nav-link" to="/download-docs">Download Docs <span>&#x25BC;</span></Link>
+
                     <Link className="nav-link" to="/profile">User Profile </Link>
 
                     <div className="dropdown">
@@ -19,9 +17,13 @@ function Header() {
                             User Management
                         </button>
                         <ul className="dropdown-menu">
+                            <li><Link className="dropdown-item" to="/create-quotation">Create New Quote</Link></li>
+                            <li><Link className="dropdown-item" to="/edit-quotations">My Quotations </Link></li>
+                            <li><Link className="dropdown-item" to="/download-docs">Download Docs </Link></li>
                             <li><Link className="dropdown-item" to="/adduser">Add User</Link></li>
-                            <li><Link className="dropdown-item" to="/edituser/id">Edit User</Link></li>
+                            {/* <li><Link className="dropdown-item" to="/edituser/id">Edit User</Link></li> */}
                             <li><Link className="dropdown-item" to="/users">All User Info</Link></li>
+                            {/* <span>&#x25BC;</span> */}
                         </ul>
                     </div>
                 </div>
